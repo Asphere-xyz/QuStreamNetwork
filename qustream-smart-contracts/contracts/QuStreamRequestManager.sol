582bc87178a9ec890acaae7ad3409a389b795646
Tugay Emin <tgntrr@gmail.com>
2025-11-11 11:32:35 +0200

    remove dots

--- qustream-smart-contracts/contracts/QuStreamRequestManager.sol
+++ qustream-smart-contracts/contracts/QuStreamRequestManager.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 /// @title QuStreamRequestManager
-/// @notice Manages encryption requests for the QuStream protocol.
-/// @dev Master node processes requests and updates their status.
+/// @notice Manages encryption requests for the QuStream protocol
+/// @dev Master node processes requests and updates their status
 contract QuStreamRequestManager is Ownable {
     using Address for address payable;
 
